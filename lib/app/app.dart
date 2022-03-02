2fa9ea607fecc6d4da75e9e7a42ffc14f42aac15
Shaidur Rahman <sahidur8500@gmail.com>
2022-03-02 23:22:09 +0600

    Created app class for application layer

---
+++ lib/app/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  MyApp._internal(); //Private named constractor
+
+  static final MyApp instance =
+      MyApp._internal(); //Single instance -> singleton
+
+  factory MyApp() => instance; //factory for the class instance
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
