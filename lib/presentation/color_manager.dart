b6959fafb0b9766c21263513f997c03d012b0101
Shaidur Rahman <sahidur8500@gmail.com>
2022-03-07 11:24:19 +0600

    Added: Theme manager for the application

--- lib/presentation/color_manager.dart
+++ lib/presentation/color_manager.dart
@@ -2,9 +2,17 @@ import 'package:flutter/cupertino.dart';
 
 class ColorManager {
   static Color primary = HexColor.fromHex("#ED9728");
-  static Color darkGray = HexColor.fromHex("#525252");
-  static Color gray = HexColor.fromHex("#737477");
-  static Color lightGray = HexColor.fromHex("#9E9E9E");
+  static Color primaryWithOpacity70 = HexColor.fromHex("#B3ED9728");
+  static Color darkGrey = HexColor.fromHex("#525252");
+  static Color grey = HexColor.fromHex("#737477");
+  static Color lightGrey = HexColor.fromHex("#9E9E9E");
+
+  //new colors
+  static Color darkPrimary = HexColor.fromHex("#d17d11");
+  static Color grey1 = HexColor.fromHex("#707070");
+  static Color grey2 = HexColor.fromHex("#797979");
+  static Color white = HexColor.fromHex("#FFFFFF");
+  static Color error = HexColor.fromHex("#e61f32");
 }
 
 extension HexColor on Color {
--- lib/presentation/values_manager.dart
+++ lib/presentation/values_manager.dart
@@ -23,6 +23,11 @@ class AppPadding {
 }
 
 class AppSize {
+  static const double s1 = 1.0;
+  static const double s1_5 = 1.5;
+  static const double s2 = 2.0;
+  static const double s4 = 4.0;
+  static const double s6 = 6.0;
   static const double s8 = 8.0;
   static const double s10 = 10.0;
   static const double s12 = 12.0;
